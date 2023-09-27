0f7dfb6a941624aad42edf8a7eb3e37a784accd5
fatimaabubakar1 <fatyeemaabubakar@gmail.com>
2023-09-26 18:05:29 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a single node in the linked list
+typedef struct listint_t {
+    int data;
+    struct listint_t *next;
+} listint_t;
+
+// Function to print all elements of the linked list
+size_t print_listint(const listint_t *h)
+{
+    size_t node_count = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->data);
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
+}
+
+int main()
+{
+    // Example of creating a linked list with three elements
+    listint_t *head = NULL;
+    listint_t *second = NULL;
+    listint_t *third = NULL;
+
+    head = (listint_t *)malloc(sizeof(listint_t));
+    second = (listint_t *)malloc(sizeof(listint_t));
+    third = (listint_t *)malloc(sizeof(listint_t));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    // Print the elements of the linked list and get the count
+    size_t count = print_listint(head);
+    printf("Number of nodes: %zu\n", count);
+
+    // Don't forget to free the memory allocated for the nodes
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+The README file of 0x13 C - more singly linked lists 
